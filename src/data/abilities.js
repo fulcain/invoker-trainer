fb3a530bbaa0967ff68aacb2ed51ac8e762d2fa3
Majid Kargar <majidkargar01@gmail.com>
2024-01-06 23:58:34 +0330

    Feat: add display name for each orb

--- src/data/abilities.js
+++ src/data/abilities.js
@@ -2,41 +2,51 @@ export default [
     {
         name: "coldSnap",
         orbs: "qqq",
+        displayName:"Cold Snap"
     },
     {
         name: "emp",
         orbs: "www",
+        displayName:"E.M.P"
     },
     {
         name: "sunStrike",
         orbs: "eee",
+        displayName:"Sun Strike"
     },
     {
         name: "tornado",
         orbs: "wwq",
+        displayName:"Tornado"
     },
     {
         name: "forgeSpirit",
         orbs: "eeq",
+        displayName:"Forge Spirit"
     },
     {
         name: "iceWall",
         orbs: "qqe",
+        displayName:"Ice Wall"
     },
     {
         name: "chaosMeteor",
         orbs: "eew",
+        displayName:"Chaos Meteor"
     },
     {
         name: "alacrity",
         orbs: "wwe",
+        displayName:"Alacrity"
     },
     {
         name: "ghostWalk",
         orbs: "qqw",
+        displayName:"Ghost Walk"
     },
     {
         name: "deafeningBlast",
         orbs: "qwe",
+        displayName:"Deafening Blast"
     },
 ];
