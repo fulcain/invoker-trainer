73a92afe87ac53da70456b716e265fea7e91e4af
Majid Kargar <majidkargar01@gmail.com>
2024-01-09 20:17:32 +0330

    Fix: Help icon position on mobile

--- src/App.jsx
+++ src/App.jsx
@@ -112,7 +112,7 @@ function App() {
                 TransitionComponent={Zoom}
             >
                 <div
-                    className="fixed top-10 right-52 bg-white text-black px-4 py-2 rounded-full cursor-pointer"
+                    className="fixed top-[5%] right-[10%] bg-white text-black px-4 py-2 rounded-full cursor-pointer"
                     onClick={showHelp}
                 >
                     ?
